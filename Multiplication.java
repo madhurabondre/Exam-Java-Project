3725b33f7cfd5eb3b9ee5b2d709976c07e9e0fcd
group023 <group023@infoway.com>
2019-12-30 15:14:21 +0530

    "Multipliccation"

---
+++ Multiplication.java
@@ -0,0 +1,22 @@
+package mypackage;
+
+import java.util.Scanner;
+
+public class Multiplication 
+{
+	public static void main(String[] args) 
+	{
+		int num1, num2;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any two numbers: ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		int mul = num1*num2;
+		int div = num1/num2;
+		
+		System.out.println("Mutliplication = "+mul);
+		System.out.println("Division = "+div);
+	}
+}
