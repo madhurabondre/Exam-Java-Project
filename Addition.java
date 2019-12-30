ff5d78564c5fe4a8feffd42a9036795d78447b67
group023 <group023@infoway.com>
2019-12-30 16:54:45 +0530

    "Java Programs"

--- Addition.java
+++ Addition.java
@@ -9,14 +9,14 @@ public class Addition
 		int num1, num2;
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter any two numbers: ");
+		System.out.println("Enter any two integer numbers: ");
 		num1 = sc.nextInt();
 		num2 = sc.nextInt();
 		
 		int add = num1+num2;
 		int sub = num1-num2;
 		
-		System.out.println("Addition = "+add);
-		System.out.println("Subtraction = "+sub);
+		System.out.println("Addition is "+add);
+		System.out.println("Subtraction is "+sub);
 	}
 }
--- Multiplication.java
+++ Multiplication.java
@@ -9,14 +9,14 @@ public class Multiplication
 		int num1, num2;
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter any two numbers: ");
+		System.out.println("Enter any two integer numbers: ");
 		num1 = sc.nextInt();
 		num2 = sc.nextInt();
 		
 		int mul = num1*num2;
 		int div = num1/num2;
 		
-		System.out.println("Mutliplication = "+mul);
-		System.out.println("Division = "+div);
+		System.out.println("Mutliplication is "+mul);
+		System.out.println("Division is "+div);
 	}
 }
