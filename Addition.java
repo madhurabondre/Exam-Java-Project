2c6be775164bef8a62530394ebd6d471bcffeda7
group023 <group023@infoway.com>
2019-12-30 15:10:25 +0530

    Addition and Subtraction

--- Addition.java
+++ Addition.java
@@ -14,7 +14,9 @@ public class Addition
 		num2 = sc.nextInt();
 		
 		int add = num1+num2;
+		int sub = num1-num2;
 		
 		System.out.println("Addition = "+add);
+		System.out.println("Subtraction = "+sub);
 	}
 }
