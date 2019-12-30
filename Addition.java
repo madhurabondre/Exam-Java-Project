a28bdd7e0313cab24c6962f1a82a2d0cf2456db9
group023 <group023@infoway.com>
2019-12-30 15:01:15 +0530

    Addition

---
+++ Addition.java
@@ -0,0 +1,20 @@
+package mypackage;
+
+import java.util.Scanner;
+
+public class Addition 
+{
+	public static void main(String[] args) 
+	{
+		int num1, num2;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any two numbers: ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		int add = num1+num2;
+		
+		System.out.println("Addition = "+add);
+	}
+}
